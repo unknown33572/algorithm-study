9d482dd34d427a99d654bb6aa4f08c945d613677
S <unknown33572@gmail.com>
2023-10-26 00:03:26 +0900

    coin-count

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-def cutbar(m, n, current):
-  if current >= n:
-    return 0
-  elif current < m:
-    return 1 + cutbar(m, n, current * 2)
-  else:
-    return 1 + cutbar(m, n, current + m)
-  
-print(cutbar(3, 8, 1))
+cnt = 0;
+
+for coin_500 in range(0, 2 + 1):
+    for coin_100 in range(0, 10 + 1):
+        for coin_50 in range(0, 15 + 1):
+            for coin_10 in range(0, 15 + 1):
+                if coin_500 + coin_100 + coin_50 + coin_10 <= 15:
+                  if coin_500 * 500 + coin_100 * 100 + coin_50 * 50 + coin_10 * 10 == 1000:
+                    cnt += 1
+                    
+print(cnt)
